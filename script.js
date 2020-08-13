385810b8657f47e5fc9c899e2fa7bb4984b9cca2
Kevin C <kevincolejr@outlook.com>
2020-08-13 11:31:21 -0400

    added yaml file for CI/CD congif

--- script.js
+++ script.js
@@ -1,7 +1,3 @@
 $(document).ready(function() {
-    console.log("ready")
-    $.get("https://fi4pfa2jd4.execute-api.us-east-1.amazonaws.com/Prod/update",function(data){
-        console.log(data)
-        $("#counter").html(data)
-    })
+    console.log("hi")
 })
