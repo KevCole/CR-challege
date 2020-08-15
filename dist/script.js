c5bbd23081dfeed458627192c6481ad68933a4d0
Kevin C <kevincolejr@outlook.com>
2020-08-15 18:18:38 -0400

    added counter api gateway call

--- dist/script.js
+++ dist/script.js
@@ -1,3 +1,7 @@
 $(document).ready(function() {
-    console.log("hi")
+    console.log("ready")
+    $.get("https://fi4pfa2jd4.execute-api.us-east-1.amazonaws.com/Prod/update",function(data){
+        console.log(data)
+        $("#counter").html(data)
+    })
 })
