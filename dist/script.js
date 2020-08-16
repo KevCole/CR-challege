a0d66fd4e93897b1ad79be72fa19fb4878434387
Kevin C <kevincolejr@outlook.com>
2020-08-15 20:02:30 -0400

    commit on master

--- dist/script.js
+++ dist/script.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-    console.log("Ready!")
+    console.log("ready")
     $.get("https://fi4pfa2jd4.execute-api.us-east-1.amazonaws.com/Prod/update",function(data){
         console.log(data)
         $("#counter").html(data)
