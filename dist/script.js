5cbbff3dd1acc98610c4705ae6966f03a10959f9
Kevin C <kevincolejr@outlook.com>
2020-08-18 16:37:19 -0400

    resetting to 0

--- dist/script.js
+++ dist/script.js
@@ -1,7 +1,7 @@
-/*$(document).ready(function() {
+$(document).ready(function() {
     console.log("ready")
     $.get("https://fi4pfa2jd4.execute-api.us-east-1.amazonaws.com/Prod/update",function(data){
         console.log(data)
         $("#counter").html(data)
     })
-}*/
+})
