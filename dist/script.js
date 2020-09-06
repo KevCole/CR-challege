4b7497cba523f65b6f27822fcd8ce1fd7ed985b7
Kevin C <kevincolejr@outlook.com>
2020-09-05 21:47:06 -0400

    " "

--- dist/script.js
+++ dist/script.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
     console.log("ready")
-    $.get("https://fi4pfa2jd4.execute-api.us-east-1.amazonaws.com/Prod/update/",function(data){
+    $.get("https://fi4pfa2jd4.execute-api.us-east-1.amazonaws.com/Prod/update",function(data){
         console.log(data)
         $("#counter").html(data)
     })
